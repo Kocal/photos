7c34ad597d810e9ce035228fdeb45b51c3303783
Hugo Alliaume <hugo@alliau.me>
2022-05-10 07:42:28 +0200

    chore: setup Profiler

--- config/bundles.php
+++ config/bundles.php
@@ -10,4 +10,5 @@ return [
     Twig\Extra\TwigExtraBundle\TwigExtraBundle::class => ['all' => true],
     Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
     Symfony\UX\Turbo\TurboBundle::class => ['all' => true],
+    Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
 ];
